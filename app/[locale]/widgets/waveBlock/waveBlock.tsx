216adf28b0804796ea03b0276a2b3ca1d684dc0a
YrecPodlec <domofon57@gmail.com>
2025-11-21 11:32:25 +0300

    languages

--- app/[locale]/widgets/waveBlock/waveBlock.tsx
+++ app/[locale]/widgets/waveBlock/waveBlock.tsx
@@ -1,27 +1,26 @@
 import React from 'react';
 import styles from './waveBlock.module.scss';
 import {Title} from "@/app/[locale]/shared";
+import {useTranslations} from "next-intl";
 
 const WaveBlock = () => {
+    const t = useTranslations('navbar')
+    const text = useTranslations('aboutText')
     return (
         <>
             <section className={styles.section}>
                 <div className={styles.box}>
-                    <Title title={'ABOUT ME'}/>
+                    <Title title={t('about')}/>
                     <div className={styles.content}>
                         <p>
-                            Four years ago, I tried my hand at web development for the first time at university. Honestly,
-                            at that time I thought I would go to game dev, but the code for websites suddenly tightened.
+                            {text('first')}
                             {<br/>}
                             {<br/>}
                             {<br/>}
-                            Since then, {`I've`} been expanding my horizons: from the first simple pages,
-                            {`I've`} moved on to a full-fledged workbench, trying different technologies and approaches to
-                            creating interfaces.
+                            {text('second')}
                         </p>
                         <span>
-                    {`I'm currently studying at my second university, testing new techniques and frameworks, 
-                        and constantly looking for fresh ways to make the web faster, more convenient, and more beautiful.`}
+                            {text('third')}
                 </span>
                     </div>
                 </div>
